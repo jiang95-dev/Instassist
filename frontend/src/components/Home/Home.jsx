6d8aedaedade0a7a81e727cc61ae0a9a94eb90b0
Jintao <jjiang43@illinois.edu>
2017-12-09 01:36:50 -0600

    home page finished

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Card } from 'semantic-ui-react'
+import { Button, Input } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 import styles from './styles.scss'
@@ -7,26 +7,33 @@ import styles from './styles.scss'
 class Home extends Component {
     render() {
         return(
-            <div className="Home">
-                <Card>
-                    <h1>Welcome to MP2!</h1>
+            <div className="background">
+                <div className="Home">
+                    <div>
+                        <h1 className="logo">Groupin</h1>
+                        <p className="title">Your Online Project Billboard</p>
+                        <input type="text" placeholder="Finding a project?" className="myInput"/>
+                    </div>
+                    <div className="stick"></div>
+                    <p className="footer-title">Project Name</p>
+                    <div>
+                        <i class="trophy icon big"></i>
+                        <i class="trophy icon big"></i>
+                        <i class="trophy icon big"></i>
+                        <i class="trophy icon big"></i>
+                    </div>
+                    <p className="footer-body">This is a random string. It makes no sense. Please replace the</p>
+                    <p>sentence with what you want.</p>
+                    <div className="stick"></div>
 
-                    <span>
-                        <Link to="/login">
-                            <Button>
-                                Login
-                            </Button>
-                        </Link>
-
-                        <Link to="/register">
-                            <Button>
-                                Register
-                            </Button>
-                        </Link>
-                    </span>
-
-                    <br />
-                </Card>
+                    
+                </div>
+                <Link to="/dashboard">
+                    <div className="continue">
+                        <p className="title continue-text">Continue</p>
+                        <i class="triangle right icon big myTriangle"></i>
+                    </div>
+                </Link>
             </div>
         )
     }
