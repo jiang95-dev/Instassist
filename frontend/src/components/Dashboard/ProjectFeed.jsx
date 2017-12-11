2d42347371ed41976306d0491cecefc20a163e04
Zhengdai Hu (David) <hu.zhengdai@gmail.com>
2017-12-11 00:02:09 -0600

    Clean up code

--- frontend/src/components/Dashboard/ProjectFeed.jsx
+++ frontend/src/components/Dashboard/ProjectFeed.jsx
@@ -3,13 +3,13 @@ import { Card, Grid, Message } from 'semantic-ui-react'
 
 import style from './project-feed.scss'
 
-const ProjectFeed = ({style, projects}) => {
+const ProjectFeed = ({ style, projects }) => {
     let feed;
-    if (projects) { 
+    if (projects) {
         feed = projects.map(project => {
             return (
                 <Grid.Column key={project} computer={4} mobile={16}>
-                    <Card 
+                    <Card
                         header={project.name}
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem ipsum, gravida sit amet purus efficitur, placerat finibus augue. Sed vel sem sed arcu imperdiet pharetra ut non felis. In vel porttitor lorem. Quisque fringilla tellus sit amet justo imperdiet, varius malesuada eros hendrerit. "
                     />
@@ -17,9 +17,9 @@ const ProjectFeed = ({style, projects}) => {
             )
         });
     } else {
-        feed = <Message 
-        header='No messages yet :)' 
-        content='Come back later for more messages!' />
+        feed = <Message
+            header='No messages yet :)'
+            content='Come back later for more messages!' />
     }
 
     return (
@@ -28,10 +28,10 @@ const ProjectFeed = ({style, projects}) => {
             <Grid className="row" columns="equal">
                 <Grid.Column key="add" computer={4} mobile={16}>
                     <Card>
-                        <p style={{height: '265px', fontSize: '50pt', lineHeight: '250px', textAlign: 'center', verticalAlign: 'center'}}> + </p>
+                        <p style={{ height: '265px', fontSize: '50pt', lineHeight: '250px', textAlign: 'center', verticalAlign: 'center' }}> + </p>
                     </Card>
                 </Grid.Column>
-                    {feed}
+                {feed}
             </Grid>
         </section>
     )
