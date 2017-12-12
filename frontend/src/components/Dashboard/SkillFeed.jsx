92a5fc52e76e5a2cc91a15b0070ede458c193890
Zhengdai Hu (David) <hu.zhengdai@gmail.com>
2017-12-11 22:59:30 -0600

    Connect to backend

--- frontend/src/components/Dashboard/SkillFeed.jsx
+++ frontend/src/components/Dashboard/SkillFeed.jsx
@@ -6,29 +6,16 @@ import style from './skill-feed.scss'
 class SkillFeed extends Component {
     constructor(props) {
         super(props);
-
         this.state = { 
-            newSkill: ' + ', 
-            skills : props.skills
+            newSkill: '', 
         };
     }
 
-    addSkill(isEnter) {
-        console.log(isEnter);
-        if (isEnter) {
-            this.setState({ 
-                skills: this.state.skills.concat(this.state.newSkill),
-                newSkill: ' + ' 
-            });
-        }
-    }
-
     render() {
-        if (this.state.skills) {
-            console.log(this.state.skills);
-            var labels = this.state.skills.map(skill => {
+        if (this.props.skills) {
+            var labels = this.props.skills.map(skill => {
                 return (
-                    <Label className="my-label">&nbsp;{skill}&nbsp;</Label>
+                    <Label key={skill} className="my-label">&nbsp;{skill}&nbsp;</Label>
                 )
             });
         }
@@ -40,11 +27,17 @@ class SkillFeed extends Component {
                     {labels}
                     <Label className="new" >
                         <input 
+                            className="new-skill"
                             value={this.state.newSkill} 
                             maxLength="15"
-                            onClick={() => this.setState({ newSkill: '' })}
+                            //onClick={() => this.setState({ newSkill: '' })}
                             onChange={e => this.setState({ newSkill: e.target.value })} 
-                            onKeyPress={e => this.addSkill(e.key === 'Enter')}
+                            onKeyPress={e => {
+                                if (e.key === 'Enter' && this.state.newSkill.trim()) {
+                                    this.props.addSkill(this.state.newSkill)
+                                    this.setState({ newSkill : '' })
+                                }
+                            }}
                             type="text" 
                         />
                     </Label>
